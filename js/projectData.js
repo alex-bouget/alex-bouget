153759efc661b0f679309750d7e9e9dc6a15b4ca
MisterMine01 <pokemovie06@gmail.com>
2022-10-16 16:58:58 +0200

    continue

--- js/projectData.js
+++ js/projectData.js
@@ -1,6 +1,5 @@
 let tagsData = {
     "Date": [
-        "2016",
         "2017",
         "2018",
         "2019",
@@ -26,27 +25,44 @@ let tagsData = {
 
 let ProjectData = [
     {
-        "name": "Atome",
-        "description": "Test du SpeechSynthesis avec questions/réponses",
-        "tags": ["2016", "BATCH", "Personnelle"],
+        "name": "ClimatView",
+        "description": "SAE 2.4 et 2.5, 2022",
+        "tags": ["2022", "Python", "SAE", "IUT"],
+        "theme": "theme1"
+    },
+    {
+        "name": "FDS",
+        "description": "Systeme de mise à jour<br>(File Download System)",
+        "tags": ["2020", "2021", "Python", "PHP", "Personnelle"],
+        "theme": "theme1"
+    },
+    {
+        "name": "Relic Engine",
+        "description": "Moteur de jeu 2D, avec ajout de script",
+        "tags": ["2019", "2021", "Python", "Personnelle"],
+        "theme": "theme1"
+    },
+    {
+        "name": "Tchat",
+        "description": "Tchat en ligne fonctionnant avec ajax",
+        "tags": ["2019", "Python", "PHP", "Personnelle"],
+    },
+    {
+        "name": "Utopia",
+        "description": "Jeu de carte multijoueur",
+        "tags": ["2018", "2019", "2020", "2021", "2022", "Python", "WEB", "PHP", "Personnelle"],
         "theme": "theme1"
     },
     {
         "name": "Launcher",
         "description": "Création d'un lanceur d'application avec connexion",
+        "tags": ["2018", "BATCH", "Personnelle"],
+        "theme": "theme1"
+    },
+    {
+        "name": "Aie",
+        "description": "Test du SpeechSynthesis avec questions/réponses",
         "tags": ["2017", "BATCH", "Personnelle"],
-        "theme": "theme1"
-    },
-    {
-        "name": "Utopia",
-        "description": "Jeu de carte multijoueur en ligne, 2019-2022",
-        "tags": ["Python", "PHP", "WEB", "2019", "2020", "2021", "2022", "Personnelle"],
-        "theme": "theme1"
-    },
-    {
-        "name": "ClimatView",
-        "description": "SAE 2.4 et 2.5, 2022",
-        "tags": ["Python", "SAE", "2022", "IUT"],
         "theme": "theme1"
     },
 ]
