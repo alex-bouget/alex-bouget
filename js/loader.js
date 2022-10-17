28862361fb373ce09b5b0c5ca6579899eccd5317
MisterMine01 <pokemovie06@gmail.com>
2022-10-17 20:33:39 +0200

    move project

--- js/loader.js
+++
@@ -1,9 +0,0 @@
-let Loader = new class {
-    load(theme, data, container) {
-        var dat = themeData[theme];
-        for (var key in data) {
-            dat = dat.replace("{" + key + "}", data[key]);
-        }
-        container.innerHTML = "<div class=\"" + theme + "\">" + dat + "</div>";
-    }
-}();
